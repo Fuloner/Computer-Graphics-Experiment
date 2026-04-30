b256f5ac098dc1c6d14aedda5dff9bc9c6854297
Fuloner <1831910387@qq.com>
2026-04-30 14:27:59 +0800

    Computer Graphics Experiment1

--- LearnOpenGL/src/imgui/imgui_demo.d
+++
@@ -1,2 +0,0 @@
-src/imgui/imgui_demo.o: src/imgui/imgui_demo.cpp src/imgui/imgui.h \
- src/imgui/imconfig.h
--- LearnOpenGL/src/imgui/imgui_impl_glfw.d
+++
@@ -1,3 +0,0 @@
-src/imgui/imgui_impl_glfw.o: src/imgui/imgui_impl_glfw.cpp \
- src/imgui/imgui.h src/imgui/imconfig.h src/imgui/imgui_impl_glfw.h \
- include/GLFW/glfw3.h include/GLFW/glfw3native.h
